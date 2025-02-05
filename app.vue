baa8563e011888f35fd9476e0fa1b0c7d77f0a46
ruslan-byte <rbaroev0@gmail.com>
2025-02-05 13:47:18 +0300

    тест автовыката

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur hic
+    doloribus, maiores quas minus totam blanditiis consequuntur, voluptatem
+    omnis, inventore voluptatibus accusamus iste qui? Culpa aliquam sunt in
+    dolore minima.
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
