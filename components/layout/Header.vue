a395055a2b6562875b168ed36f7b5b0cd380284b
Pavel251001 <pak.pavel2017@yandex.ru>
2025-02-06 21:27:43 +0300

    pavel

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,10 +1,8 @@
 <template>
-<<<<<<< HEAD
+
   <header>
     <P>Как дела?</P>
   </header>
-=======
-  <div>я тут что-то написал</div>
->>>>>>> 1ec22cb37530fc509b2acd2c3e8005af5a1c27e4
+
 </template>
 <style></style>
