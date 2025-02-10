f6e106c1277bee9476d7e19e42ff615b7afd421a
Nikita-Apandov <apandovnikita@gmail.com>
2025-02-10 13:56:10 +0300

    nikita2

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -2,7 +2,7 @@
 
   <footer>
     <div class="footer1" >
-      <a href="#" id="back-to-top" class="back-to-top" title="Back to top"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <a href="#" id="back-to-top" class="back-to-top" title="Back to top"><br><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M18 15L12 9L6 15" stroke="#1D1D1B" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
  В начало страницы</a>
@@ -49,7 +49,7 @@
       <ul class="spisoc5">
       <li><a href="mailto:unknown@raccoon.ru">&#9426; 1998 - 2020 ООО &laquo;Канцпарк&raquo;</a></li>
       <li><p>Все права защищены</p></li>
-      <li><svg class="svg1" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <li><a href="mailto:unknown@raccoon.ru"><svg class="svg1" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <circle cx="16" cy="16" r="16" fill="url(#paint0_linear_1_1145)"/>
 <path d="M19.6377 7H12.3623C9.40551 7 7 9.40551 7 12.3623V19.6377C7 22.5945 9.40551 25 12.3623 25H19.6377C22.5945 25 25 22.5945 25 19.6377V12.3623C25 9.40551 22.5945 7 19.6377 7ZM23.1892 19.6377C23.1892 21.5991 21.5991 23.1892 19.6377 23.1892H12.3623C10.4009 23.1892 8.8108 21.5991 8.8108 19.6377V12.3623C8.8108 10.4008 10.4009 8.8108 12.3623 8.8108H19.6377C21.5991 8.8108 23.1892 10.4008 23.1892 12.3623V19.6377Z" fill="white"/>
 <path d="M16.0002 11.3445C13.4332 11.3445 11.3447 13.4329 11.3447 15.9999C11.3447 18.5669 13.4332 20.6553 16.0002 20.6553C18.5672 20.6553 20.6556 18.5669 20.6556 15.9999C20.6556 13.4329 18.5672 11.3445 16.0002 11.3445ZM16.0002 18.8446C14.4291 18.8446 13.1555 17.571 13.1555 15.9999C13.1555 14.4289 14.4291 13.1553 16.0002 13.1553C17.5712 13.1553 18.8448 14.4289 18.8448 15.9999C18.8448 17.571 17.5712 18.8446 16.0002 18.8446Z" fill="white"/>
@@ -62,17 +62,17 @@
 <stop offset="1" stop-color="#7024C4"/>
 </linearGradient>
 </defs>
-</svg>
+</svg></a>
 
-      <svg class="svg2" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<a href="mailto:unknown@raccoon.ru"><svg class="svg2" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z" fill="#4D76A1"/>
 <path fill-rule="evenodd" clip-rule="evenodd" d="M15.3962 23.0173H16.652C16.652 23.0173 17.0313 22.9757 17.225 22.7669C17.4032 22.5752 17.3975 22.2153 17.3975 22.2153C17.3975 22.2153 17.373 20.5302 18.1551 20.2821C18.926 20.0377 19.916 21.9107 20.9653 22.6308C21.7588 23.1759 22.3617 23.0564 22.3617 23.0564L25.1674 23.0173C25.1674 23.0173 26.6351 22.9269 25.9392 21.7729C25.8821 21.6785 25.5339 20.9193 23.8531 19.3591C22.0939 17.7263 22.3295 17.9904 24.4486 15.1659C25.7392 13.4458 26.2552 12.3956 26.094 11.9458C25.9403 11.5174 24.9911 11.6307 24.9911 11.6307L21.8321 11.6503C21.8321 11.6503 21.5979 11.6184 21.4242 11.7222C21.2545 11.824 21.1453 12.0613 21.1453 12.0613C21.1453 12.0613 20.6453 13.3924 19.9785 14.5244C18.5718 16.9131 18.0093 17.0392 17.7794 16.8909C17.2447 16.5452 17.3782 15.5022 17.3782 14.7612C17.3782 12.4464 17.7292 11.4812 16.6945 11.2314C16.3511 11.1484 16.0984 11.0936 15.2202 11.0848C14.093 11.0731 13.139 11.0882 12.5988 11.3529C12.2394 11.5288 11.9622 11.921 12.131 11.9435C12.3398 11.9715 12.8127 12.071 13.0634 12.4124C13.3871 12.8528 13.3757 13.8419 13.3757 13.8419C13.3757 13.8419 13.5617 16.5669 12.9413 16.9054C12.5155 17.1376 11.9314 16.6636 10.6773 14.4965C10.0347 13.3864 9.54956 12.1594 9.54956 12.1594C9.54956 12.1594 9.45601 11.9301 9.28916 11.8075C9.08666 11.6589 8.80373 11.6115 8.80373 11.6115L5.80184 11.6312C5.80184 11.6312 5.3512 11.6438 5.18577 11.8397C5.0386 12.014 5.17408 12.3745 5.17408 12.3745C5.17408 12.3745 7.52425 17.8729 10.1853 20.6437C12.6253 23.1841 15.3962 23.0173 15.3962 23.0173Z" fill="white"/>
-</svg>
+</svg></a>
 
-      <svg class="svg3" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<a href="mailto:unknown@raccoon.ru"><svg class="svg3" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z" fill="#3B5998"/>
 <path d="M22.5708 18.4192H19.7158V32H15.3903V18.4192H13.333V14.7433H15.3903V12.3646C15.3903 10.6636 16.1983 8 19.7543 8L22.9584 8.0134V11.5814H20.6337C20.2523 11.5814 19.7161 11.772 19.7161 12.5834V14.7468H22.9487L22.5708 18.4192Z" fill="white"/>
-</svg>
+</svg></a>
 </li>
       </ul>
   
@@ -86,44 +86,52 @@
 </template>
 <script setup lang="ts"></script>
 <style>
+body{
+  margin: 0;
+}
 footer {
   background-color: blueviolet;
   
-  height: 300px;
+  height: 290px;
   width: auto;
   margin: 0px;
 
 }
 .footer1 {
-  background-color: antiquewhite;
+  
+  
+  background-color: #fbfbfa;
   margin: 0px;
   height: 72px;
   text-align: center;
+  font-weight: bold;
+  
  
 }
 .footer2 {
-  background-color: aliceblue;
+  background-color: #f6f6f6;
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items:top;
   margin: 0px;
-  height: 265px;
-
+  height: 205px;
+  padding-top: 20px;
+  gap: 40px;
 }
 .bloc1 {
-  width: 160px;
+  width: 100px;
   
 }
 .bloc2 {
-  width: 160px;
+  width: 138px;
   
 }
 .bloc3 {
-  width: 190px;
+  width: 226px;
   
 }
 .bloc4 {
-  width: 160px;
+  width: 117px;
   
 }
 .bloc5 {
@@ -131,30 +139,68 @@ footer {
   
 }
 .spisoc1 {
+  font-size: 16px;
+
+  line-height: 25px;
+  width: 96px;
+  font-weight: bold;
   list-style: none;
   text-align: left;
 }
 .spisoc2 {
+  font-size: 16px;
+  line-height: 25px;
+  width: 230px;
+  font-weight: bold;
   list-style: none;
   text-align: left;
 }
 .spisoc3 {
+  font-size: 16px;
+  line-height: 25px;
+  width: 230px;
+  font-weight: bold;
   list-style: none;
   text-align: left;
 }
 .spisoc4 {
+  font-size: 16px;
+  line-height: 25px;
+  width: 230px;
+  font-weight: bold;
   list-style: none;
   text-align: left;
 }
 .spisoc5 {
+  width: 230px;
+  font-size: 16px;
+  color:#727272;
   text-align: left;
   list-style: none;
 }
 a {
+  font-weight: normal;
   text-decoration: none;
   color: #727272;
 }
-
+.back-to-top {
+  
+  color: #1D1D1B;
+  font-weight: bold;
+  
+}
+.svg1 {
+  margin-right: 10px;
+}
+.svg2 {
+  margin-right: 10px;
+}
+.svg3 {
+  margin-right: 10px;
+}
+a:hover { 
+  color: #1D1D1B;
+}
 
    
 </style>
