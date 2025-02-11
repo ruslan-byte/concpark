e702cbaa0ebcf096f509b0a4b832c044f1ae9f62
Nikita-Apandov <apandovnikita@gmail.com>
2025-02-11 18:49:11 +0300

    content_nikita

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
+
 <template>
-  <div></div>
+  <div>
+    <LayoutTovarone></LayoutTovarone>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LayoutTovar } from '#components';
+
+</script>
+
 <style></style>
+
+
