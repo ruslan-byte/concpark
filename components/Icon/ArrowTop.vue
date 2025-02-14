984f3db57a57e7c005ca5d6ca22e9c8abcde32d0
Nikita-Apandov <apandovnikita@gmail.com>
2025-02-14 15:05:03 +0300

    footer_container_nikita

---
+++ components/Icon/ArrowTop.vue
@@ -0,0 +1,19 @@
+<template>
+<svg 
+        width="24" 
+        height="24" 
+        viewBox="0 0 24 24" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+        >
+        <path   
+          d="M18 15L12 9L6 15" 
+          stroke="#1D1D1B" 
+          stroke-width="2" 
+          stroke-linecap="round" 
+          stroke-linejoin="round"
+          />
+        </svg>
+        </template>
+        <style>
+        </style>
