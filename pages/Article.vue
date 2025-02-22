e978cab64ba5988859d110e1747014c2d49351de
Nikita-Apandov <apandovnikita@gmail.com>
2025-02-22 21:13:10 +0300

    article_nikita

--- pages/Article.vue
+++ pages/Article.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="article">
-    <div class="articleone"> <!--первая часть статьи из четырёх крурных элементов-->
-      <h1 class="articleheder">Rак правильно выбрать канцтовары</h1>
-      <div class="articleblock">
-        <div class="articleblockone"><!--контейнер первого элемента первой части-->
-          <div class="articleblockoneimg"><img src="https://i.ibb.co/0jnYCRLR/Mask-Group.png" alt="Письменный стол" width="832px"
-            height="297px"></div><!--элемент большой фотки-->
-          <div class="articleblockoneblockicon"><!--контейнер для иконок под большой фоткой-->
-            <div class="articleblockoneblockiconone"><IconHeart></IconHeart></div><!--элемент первой иконки-->
-            <div class="articleblockoneblockicontwo"><IconEye></IconEye></div><!--элемент второй иконки-->
-            <div class="articleblockoneblockiconthree">Поделиться</div><!--элемент третьей иконки-->
-            <div class="articleblockoneblockiconfour"><IconVkarticle></IconVkarticle></div><!--элемент четвертой иконки-->
-            <div class="articleblockoneblockiconfive"><IconFacebookarticle></IconFacebookarticle></div><!--элемент пятой иконки-->
-            <div class="articleblockoneblockiconsix"><IconInstaarticle></IconInstaarticle></div><!--элемент шестой иконки-->
-            <div class="articleblockoneblockiconseven"><IconOK></IconOK></div><!--элемент седьмой иконки-->
+    <div class="container">
+      <div class="article__heder"><h1>Как правильно выбрать канцтовары</h1></div>
+      <div class="article__block-flex">
+        <div class="block-one">
+          <div class="block-one__img"><img src="https://i.ibb.co/0jnYCRLR/Mask-Group.png" alt="Письменный стол" width="832px"
+            height="297px">
           </div>
-          <div class="articleblockonetext"><p>Офис любой компании невозможно представить без канцелярских принадлежностей. С одной стороны, офисная канцелярия — это стиль и имидж компании, а с другой — весомая статья расходов. Чтобы достичь золотой середины, нужно грамотно подойти к вопросу выбора товаров для офиса. Бумага — самый частый расходный материал. Нужно правильно рассчитать, сколько сотрудникам необходимо бумаги, и какого формата – А4 или А3. Кроме этого необходимо купить бумагу для написания заметок.</p>
+          <div class="block-one__element">
+            <div class="icon">
+              <div class="icon__flex">
+                <div class="icon__one"><IconHeart></IconHeart></div><!--элемент первой иконки-->
+                <div class="icon__two"><IconEye></IconEye></div><!--элемент второй иконки-->
+                <div class="icon__three">Поделиться</div><!--элемент третьей иконки-->
+                <div class="icon__four"><IconVkarticle></IconVkarticle></div><!--элемент четвертой иконки-->
+                <div class="icon__five"><IconFacebookarticle></IconFacebookarticle></div><!--элемент пятой иконки-->
+                <div class="icon__six"><IconInstaarticle></IconInstaarticle></div><!--элемент шестой иконки-->
+                <div class="icon__seven"><IconOK></IconOK></div><!--элемент седьмой иконки-->
+              </div>
+            </div>  
+          </div>
+          <div class="block-one__text">
+                                           <p>Офис любой компании невозможно представить без канцелярских принадлежностей. С одной стороны, офисная канцелярия — это стиль и имидж компании, а с другой — весомая статья расходов. Чтобы достичь золотой середины, нужно грамотно подойти к вопросу выбора товаров для офиса. Бумага — самый частый расходный материал. Нужно правильно рассчитать, сколько сотрудникам необходимо бумаги, и какого формата – А4 или А3. Кроме этого необходимо купить бумагу для написания заметок.</p>
                                            <p>Все важные документы требуют обязательного хранения в хороших условиях, при обеспечении быстрого доступа к ним. С такой задачей отлично справляются папки различных типов, файлы, боксы для хранения бумаг.</p>
                                            <p>Письменные принадлежности:</p>
                                            <ul>
@@ -26,182 +32,244 @@
                                            </ul>
                                            <p>Они расходуются быстрее всего. Данные принадлежности должны обладать высоким качеством и доступной ценой. Еще одна группа товаров состоит из скрепок, кнопок, различных папок, блокнотов, линеек, ножниц, точилки для карандашей и т.д. Это более длительно расходуемый и используемый товар.</p>
                                            <p>Канцелярские принадлежности по праву можно назвать незаменимыми помощниками во всех сферах жизнедеятельности человека. Правильный выбор канцтоваров предоставит возможность сделать работу, учебу более организованной.</p>
-                                           <p>12.03.2020</p></div><!--текс первого блока-->
-        </div>        
-        <div class="articleblockgrid"><!--контейнер grid для третьего и четвертого элемента-->
-          <div class="articleblockthree"><!--конрейнер третьего элемента первой части-->
-            <div class="articleblockthreeimg"><img src="https://i.ibb.co/qFhr6Gkv/4d7991782de6630e913cafdbfc69b016-1.png" alt="Подставка под карандаши"></div><!--изображение третьего элемента-->
-            <div class="articleblockthreetext"><p class="textbold">ТОП-10 оригинальных подставок для карандашей</p>
+                                           <p>12.03.2020</p>
+          </div><!--текс первого блока-->
+        </div>
+        <div class="block-two">
+          <div class="block-two__grid">
+            <div class="block-two__column">
+              <div class="item-article">
+                <div class="item-article__column">
+                <div class="item-article__img"><img src="https://i.ibb.co/qFhr6Gkv/4d7991782de6630e913cafdbfc69b016-1.png" alt="Подставка под карандаши">
+                </div>
+                <div class="item-article__text"><p class="text-bold">ТОП-10 оригинальных подставок для карандашей</p>
                                                <p>Где только не предлагают хранить пишущие принадлежности пытливые умы современных...</p>
-                                               <p class="blockfourtextdate">05.03.2020</p></div><!--текс третьего элемента-->
-          </div>
-          <div class="articleblockfour"><!--контейнер четвертого элемента первой части-->
-            <div class="articleblockfourimg"><img src="https://i.ibb.co/gZqF0Rt7/35a4ec1b1f2a7960e315f591d06f13e4-1.png" alt="Ручки гаджеты"></div><!--изображение четвертого элемента-->
-            <div class="articleblockfourtext"><p class="textbold">Пятерка удивительных ручек-гаджетов</p>
+                                               <p class="text-date">05.03.2020</p>
+                </div>
+                </div>
+              </div>
+              <div class="item-article">
+                <div class="item-article__column">
+                <div class="item-article__img"><img src="https://i.ibb.co/gZqF0Rt7/35a4ec1b1f2a7960e315f591d06f13e4-1.png" alt="Ручки гаджеты">
+                </div>
+                <div class="item-article__text"><p class="text-bold">Пятерка удивительных ручек-гаджетов</p>
                                                <p>В современном мире писать «от руки» человеку приходится все реже. Но приходится. Предлагаю...</p>
-                                               <p class="blockfourtextdate">27.02.2020</p></div><!--текст четрвертого элемента-->
+                                               <p class="text-date">27.02.2020</p>
+                </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
-      </div>  
-    </div>
-    <div class="articletwo"><!--блок второй части статьи-->
-      <div class="articletwoheder">Товары, упомянутые в теме</div><!--заголовок второй части-->
-      <div class="articletwoblockflex"><!--контейнер для элементов второй части-->
-        <div class="articletwoblockone"><!--первый элемент второй части может придется пошаманить с названиями классос-->
-          <div class="articletwoblockoneimg"><img class="img" src="https://i.ibb.co/htLsJjs/27af326bd2426805ff503e92715f151d-1.png" alt="Гуашь"></div>
-          <div class="articletwoblockonestar"> <IconStar></IconStar></div>
-          <div class="articletwoblockonetext">  <p>Гуашь Гамма "Классическая", 12 цветов, 20мл, картон</p><p class="textbold">170,69 ₽</p></div>
-          <div class="articletwoblockonebutton">
-            <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
-            <button type="button" name="popup-button" class="basket">В Корзину</button>
-          </div>  
-        </div>
-        <div class="articletwoblockone"><!--второй элемент второй части может придется пошаманить с названиями классос-->
-          <div class="articletwoblockoneimg"><img class="img" src="https://i.ibb.co/3yBrzXym/fe0dc24cd401cac27ac7c8dc884af7da-1.png" alt="Тесто для лепки"></div>
-          <div class="articletwoblockonestar"> <IconStar></IconStar></div>
-          <div class="articletwoblockonetext">  <p>Тесто для лепки Гамма "Малыш", 08 цветов*25г, формочка</p><p class="textbold">106,65 ₽</p></div>
-          <div class="articletwoblockonebutton">
-            <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
-            <button type="button" name="popup-button" class="basket">В Корзину</button>
-          </div>  
-        </div>
-        <div class="articletwoblockone"><!--третий элемент второй части может придется пошаманить с названиями классос-->
-          <div class="articletwoblockoneimg"><img class="img" src="https://i.ibb.co/RpfPsPTG/05a68d9db9aa45c10f24b644cdc02932-1.png" alt="Настольная подставка"></div>
-          <div class="articletwoblockonestar"> <IconStar></IconStar></div>
-          <div class="articletwoblockonetext">  <p>Настольная подставка Berlingo "Steel&Style", металлическая, 3 секции, черная</p><p class="textbold">337,32 ₽</p></div>
-          <div class="articletwoblockonebutton">
-            <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
-            <button type="button" name="popup-button" class="basket">В Корзину</button>
-          </div>  
-        </div>
-        <div class="articletwoblockone"><!--четвертый элемент второй части может придется пошаманить с названиями классос-->
-          <div class="articletwoblockoneimg"><img class="img" src="https://i.ibb.co/N64msQmZ/f8b36527a3dbc27ffb3ccd8afc2e5b0b-1.png" alt="f8b36527a3dbc27ffb3ccd8afc2e5b0b-1"></div>
-          <div class="articletwoblockonestar"> <IconStar></IconStar></div>
-          <div class="articletwoblockonetext">  <p>Папка с зажимом Berlingo "Standard", 17мм, 700мкм, синяя</p><p class="textbold">54,68 ₽</p></div>
-          <div class="articletwoblockonebutton">
-            <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
-            <button type="button" name="popup-button" class="basket">В Корзину</button>
-          </div>  
-        </div>
-   
-  
-
-
+        
       </div>
+      <div class="article__heder"><h2>Товары, упомянутые в теме</h2></div>
+      <div class="artecle__block-flex">
+        
+        <div class="block-three">
+          <div class="block-three__flex">
+              <div class="block-three__column"><!--первый элемент второй части может придется пошаманить с названиями классос-->
+              <div class="block-three__img"><img src="https://i.ibb.co/htLsJjs/27af326bd2426805ff503e92715f151d-1.png" alt="Гуашь"></div>
+              <div class="block-three__star"> <IconStar></IconStar></div>
+              <div class="block-three__text">  <p>Гуашь Гамма "Классическая", 12 цветов, 20мл, картон</p><p class="text-bold">170,69 ₽</p></div>
+              <div class="block-three__button">
+                <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
+                <button type="button" name="popup-button" class="basket">В Корзину</button>
+              </div>  
+            </div>
+            <div class="block-three__column"><!--второй элемент второй части может придется пошаманить с названиями классос-->
+              <div class="block-three__img"><img src="https://i.ibb.co/3yBrzXym/fe0dc24cd401cac27ac7c8dc884af7da-1.png" alt="Тесто для лепки"></div>
+              <div class="block-three__star"> <IconStar></IconStar></div>
+              <div class="block-three__text">  <p>Тесто для лепки Гамма "Малыш", 08 цветов*25г, формочка</p><p class="text-bold">106,65 ₽</p></div>
+              <div class="block-three__button">
+                <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
+                <button type="button" name="popup-button" class="basket">В Корзину</button>
+              </div>  
+            </div>
+            <div class="block-three__column"><!--третий элемент второй части может придется пошаманить с названиями классос-->
+              <div class="block-three__img"><img src="https://i.ibb.co/RpfPsPTG/05a68d9db9aa45c10f24b644cdc02932-1.png" alt="Настольная подставка"></div>
+              <div class="block-three__star"> <IconStar></IconStar></div>
+              <div class="block-three__text">  <p>Настольная подставка Berlingo "Steel&Style", металлическая, 3 секции, черная</p><p class="text-bold">337,32 ₽</p></div>
+              <div class="block-three__button">
+                <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
+                <button type="button" name="popup-button" class="basket">В Корзину</button>
+              </div>  
+            </div>
+            <div class="block-three__column"><!--четвертый элемент второй части может придется пошаманить с названиями классос-->
+              <div class="block-three__img"><img src="https://i.ibb.co/N64msQmZ/f8b36527a3dbc27ffb3ccd8afc2e5b0b-1.png" alt="f8b36527a3dbc27ffb3ccd8afc2e5b0b-1"></div>
+              <div class="block-three__star"> <IconStar></IconStar></div>
+              <div class="block-three__text">  <p>Папка с зажимом Berlingo "Standard", 17мм, 700мкм, синяя</p><p class="text-bold">54,68 ₽</p></div>
+              <div class="block-three__button">
+                <button type="button" name="popup-button" class="sravnenie">Сравнить</button>
+                <button type="button" name="popup-button" class="basket">В Корзину</button>
+              </div>  
+          </div>
+        </div>
+        </div>
+        </div>
 
+      
     </div>
   </div>
 
+
 </template>
-<script setup lang="ts"></script>
 <style>
-.article {
-  margin: 0;
-  width: auto;
-  max-width: 1200px;
-  margin-left: auto;
-  margin-right: auto;
-}
-.articleone {
-}
-.articleheder {
-}
-.articleblock {
-  display: flex;
-  justify-content: space-between;
-}
-.articleblockone {
-  display: inline-block;
-  
-  width: 832px;
-  margin-right: 50px;
-}
-.articleblockoneimg {
-  
-}
-.articleblockoneblockicon {
-  display: flex;
-  width: 832px;
-}
-.articleblockoneblockiconone {
-  margin-left: auto;
-}
-.articleblockoneblockicontwo {
-  margin-left: auto;
-}
-.articleblockoneblockiconthree {
-  margin-left: auto;
-}
-.articleblockoneblockiconfour {
-  margin-right: auto;
-}
-.articleblockoneblockiconfive {
-  margin-right: auto;
 
-}
-.articleblockoneblockiconsix {
-  margin-right: auto;
-  
-}
-.articleblockoneblockiconseven {
-  margin-right: auto;
+
+
+
+.article__heder {
+font-family: 'Open Sans';
+font-style: normal;
+font-weight: 600;
 }
 
-.articleblockonetext {
+.article__block-flex {
+  display: flex;
 }
-.articleblockgrid {
-  display: grid;
-  grid-template-columns: repeat(1,260px);
+
+
+
+.block-one__img {
+  width: 832px;
+  
+}
+
+.block-one__element {
+  
+  
+}
+
+.block-one__text {
+  width: 832px;
+}
+
+.icon__flex {
+  display: flex;
+  margin: 5px;
   justify-self: end;
+}
   
+.icon__one {
+  margin-right: 10px ;
+}
+
+.icon__two {
+  margin-right:440px ;
+}
+
+
+.icon__three {
+  margin-top: 5px;
+  margin-right: 10px ;
+}
+
+.icon__four {
+  margin-right: 10px ;
+  
+}
+
+.icon__five {
+  margin-right: 10px ;
+}
+
+.icon__six {
+  margin-right: 10px ;
+}
+
+.icon__seven {
+  margin-right: 10px ;
+}
+
+
+
+.block-two__grid {
+  width: 268px;
+  margin-left: 50px;
   
   
 }
-.articleblockthree {
+
+.block-two__column {
+  display: grid;
+  gap: 30px;
+  
+}
+
+ 
+.item-article__column{
+  
+  margin-bottom: 20px;
+  
+}
+.item-article__img {
+}
+
+.item-article__text {
+}
+
+.text-bold {
+  font-weight: 700;
+}
+.text-date {
+width: 62px;
+font-family: 'Open Sans';
+font-size: 12px;
+opacity: 0.5; 
+}
+
+
+.artecle__block-flex {
   
 
 }
-.articleblockthreeimg {
-}
-.articleblockthreetext {
-}
-.textbold {
-  font-weight: bold;
-}
-.blockfourtextdate {
-}
-.articleblockfour {
-}
-.articleblockfourimg {
-}
-.articleblockfourtext {
-}
-.articletwo {
-}
-.articletwoheder {
-}
-.articletwoblockflex {
+
+
+.block-three__flex {
+  
   display: flex;
+  height: 400px;
+  width: 1170px;
   justify-content: space-between;
+     
+     
+        
+     
+}
+.block-three__column {
+  padding-left: 10px;
+  width: 256px;
+  height: 431px;
+  
+    
+     
+     
+        
+     
   
 }
-.articletwoblockone {
+
+.block-three__img {
+  height: 190px;
 }
-.articletwoblockoneimg {
+
+.block-three__star {
 }
-.img {
+
+.block-three__text {
+  height: 100px;
 }
-.articletwoblockonestar {
+
+.block-three__button {
+  
+  
 }
-.articletwoblockonetext {
-}
-.articletwoblockonebutton {
-}
+
 .sravnenie {
 }
 .basket {
+  
 }
-
 
 </style>
